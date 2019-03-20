5c4fb30c55d7ffb5acb041464d4db4fb38ed0d40
alfredomota <alfredolaureta@hotmail.com>
2019-03-20 13:15:38 +0000

    Updates

---
+++ precache-manifest.c9681dfbb85dba4df0edcb75bd9abeb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b440705a25bfc90c279a",
+    "url": "/StarWarsApi/static/css/main.97a8c973.chunk.css"
+  },
+  {
+    "revision": "b440705a25bfc90c279a",
+    "url": "/StarWarsApi/static/js/main.b22e2944.chunk.js"
+  },
+  {
+    "revision": "b39884411f74d05576bf",
+    "url": "/StarWarsApi/static/js/runtime~main.1c6fdcac.js"
+  },
+  {
+    "revision": "f5c2038806e8178eaeff",
+    "url": "/StarWarsApi/static/css/2.9cc782a8.chunk.css"
+  },
+  {
+    "revision": "f5c2038806e8178eaeff",
+    "url": "/StarWarsApi/static/js/2.6607d977.chunk.js"
+  },
+  {
+    "revision": "0784d0c2ca0e74577a686d1a8cab3fb0",
+    "url": "/StarWarsApi/static/media/star.0784d0c2.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/StarWarsApi/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "0b2b4e13a5d9d2158b74f643706293fe",
+    "url": "/StarWarsApi/index.html"
+  }
+];
